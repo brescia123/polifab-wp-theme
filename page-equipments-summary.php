aaefd149122952ade3d9ae5c33df501ace63a07f
Giacomo Bresciani <brescia123@gmail.com>
2015-05-04 18:26:56 +0200

    equipment list on two column

--- page-equipments-summary.php
+++ page-equipments-summary.php
@@ -9,29 +9,31 @@ get_header(); ?>
 				<?php get_template_part( 'content', 'page' ); ?>
 			<div class="entry-content">				
 				<h2>Equipment List</h2>
-				<?php $fields = get_field_object('field_54b5053a6447d');
-					$categories = $fields['choices'];
-				?>
-				<?php foreach ($categories as $category ) {
-					echo '<ul><h3>';
-					echo $category;
-					echo '</h3>';
-					$category_pages = get_posts(array(
-						'posts_per_page' => -1,
-					    'post_type' => 'page',
-					    'meta_key' => 'category',
-						'meta_value' => $category
-					));
-					echo '<ul>';
-						foreach ($category_pages as $page) {
-							echo '<li>';
-							// Add the link
-							echo '<a href="'.get_permalink($page->ID).'">'.$page->post_title.'</a></li>';
-						}
+				<div id="equipment-summary-list">
+					<?php $fields = get_field_object('field_54b5053a6447d');
+						$categories = $fields['choices'];
+					?>
+					<?php foreach ($categories as $category ) {
+						echo '<ul><h3>';
+						echo $category;
+						echo '</h3>';
+						$category_pages = get_posts(array(
+							'posts_per_page' => -1,
+						    'post_type' => 'page',
+						    'meta_key' => 'category',
+							'meta_value' => $category
+						));
+						echo '<ul>';
+							foreach ($category_pages as $page) {
+								echo '<li>';
+								// Add the link
+								echo '<a href="'.get_permalink($page->ID).'">'.$page->post_title.'</a></li>';
+							}
+						echo '</ul>';
 					echo '</ul>';
-				echo '</ul>';
-				}
-				?>
+					}
+					?>
+				<div>
 			</div>
 			<?php endwhile; // end of the loop. ?>
 
